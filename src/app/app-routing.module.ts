6b31317f08011b9f40d97ccbc45b6825357bf198
AlexZander25 <aledelflou@hotmail.cl>
2023-11-01 21:13:02 -0300

    actualizacion de inicio sesion y registrar tambien home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,32 +3,33 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'primera',
     pathMatch: 'full'
   },
   {
     path: 'primera',
-    loadChildren: () => import('./primera/primera.module').then( m => m.PrimeraPageModule)
-  },  {
+    loadChildren: () => import('./primera/primera.module').then(m => m.PrimeraPageModule)
+  },
+  {
     path: 'segunda',
-    loadChildren: () => import('./segunda/segunda.module').then( m => m.SegundaPageModule)
+    loadChildren: () => import('./segunda/segunda.module').then(m => m.SegundaPageModule)
   },
   {
     path: 'tercera',
-    loadChildren: () => import('./tercera/tercera.module').then( m => m.TerceraPageModule)
+    loadChildren: () => import('./tercera/tercera.module').then(m => m.TerceraPageModule)
   },
   {
     path: 'cuarta',
-    loadChildren: () => import('./cuarta/cuarta.module').then( m => m.CuartaPageModule)
+    loadChildren: () => import('./cuarta/cuarta.module').then(m => m.CuartaPageModule)
   },
   {
     path: 'quinta',
-    loadChildren: () => import('./quinta/quinta.module').then( m => m.QuintaPageModule)
+    loadChildren: () => import('./quinta/quinta.module').then(m => m.QuintaPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
 
 ];
