8f7a709aae8573246a59f0e5a8d23d99a2b1fcf7
AlexZander25 <aledelflou@hotmail.cl>
2023-11-21 23:53:10 -0300

    actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'primera',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
